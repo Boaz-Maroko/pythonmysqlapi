579d483af86206dd3b3be41d7eeb16da40cc443d
Boaz-Maroko <boazmaroko123@gmail.com>
2024-09-20 14:32:38 +0300

    added a little bit of fuctionality

--- tests/test.py
+++ tests/test.py
@@ -1,17 +1,36 @@
 from unittest import TestCase
 import mysql.connector as conn
 from api.main import Server
+import subprocess
+
+# A function that will be used to create server instances
+
+# Create a server instance
+
+def create_server_instances():
+    server_instance = Server()
+    return server_instance
+
+
+
+
 
 class ServerTests(TestCase):
-    def test_server_callables(self):
-        config = {
-            "user": "pycode",
-            "password": "3edFA8ga&&",
-            "host": "localhost",
-        }
-        server = Server(config)
+    # I try to make the test names as simple as possible
 
-        self.assertEqual(str(server), 'localhost')
-        
-        
+    def test_server_name(self):
+        server = create_server_instances()
+        server
+    
+    def test_server_start(self):
+        server = create_server_instances()
+        self.assertEqual(True, server.start())
+    
+    def test_whether_the_server_is_running(self):
+        server = Server().start()
+        # start_server = server.start()
+        self.assertIs(True, server)
 
+    def test_server_stopped(self):
+        server = create_server_instances()
+        self.assertIs(True, server.stop())
